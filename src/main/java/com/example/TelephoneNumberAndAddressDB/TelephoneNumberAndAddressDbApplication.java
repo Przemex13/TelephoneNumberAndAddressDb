604f2d422d7a8077e706f549f97ffe9fb42f667e
Przemyslaw Poskrobko <przemyslaw.poskrobko@gmail.com>
2024-05-02 13:19:00 +0200

    init commit

---
+++ src/main/java/com/example/TelephoneNumberAndAddressDB/TelephoneNumberAndAddressDbApplication.java
@@ -0,0 +1,14 @@
+package com.example.TelephoneNumberAndAddressDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//docker run --name springBootCrudDB -e MYSQL_ROOT_PASSWORD=Przemek123 -e MYSQL_DATABASE=telephoneAndAddressDB -d -p 3307:3306 mysql
+@SpringBootApplication
+public class TelephoneNumberAndAddressDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelephoneNumberAndAddressDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TelephoneNumberAndAddressDB/TelephoneNumberAndAddressDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TelephoneNumberAndAddressDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephoneNumberAndAddressDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
