604a71b2f9b374cae999d56bd546cf64cbdbb9d2
Przemyslaw Poskrobko <przemyslaw.poskrobko@gmail.com>
2024-05-04 13:10:05 +0200

    personal_details_form.html prawie gotowy, bez województw, zastosowany lombok

--- src/main/java/com/example/TelephoneNumberAndAddressDB/personalDetails/PersonalDetail.java
+++ src/main/java/com/example/TelephoneNumberAndAddressDB/personalDetails/PersonalDetail.java
@@ -1,7 +1,11 @@
 package com.example.TelephoneNumberAndAddressDB.personalDetails;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table (name = "personal_details")
 public class PersonalDetail {
@@ -24,86 +28,42 @@ public class PersonalDetail {
     private String postcode;
     @Column(length = 20, name = "city")
     private String city;
-//    @Enumerated(EnumType.STRING)
-//    private Voivodeship voivodeship;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getPeselNumber() {
-        return peselNumber;
-    }
-
-    public void setPeselNumber(String peselNumber) {
-        this.peselNumber = peselNumber;
-    }
-
-    public String getTelephoneNumber() {
-        return telephoneNumber;
-    }
-
-    public void setTelephoneNumber(String telephoneNumber) {
-        this.telephoneNumber = telephoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        street = street;
-    }
-
-    public String getPostcode() {
-        return postcode;
-    }
-
-    public void setPostcode(String postcode) {
-        this.postcode = postcode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-//    public Voivodeship getVoivodship() {
-//        return voivodeship;
+//    public void setId(int id) {
+//        this.id = id;
 //    }
 //
-//    public void setVoivodship(Voivodeship voivodeship) {
-//        this.voivodeship = voivodeship;
+//    public void setName(String name) {
+//        this.name = name;
 //    }
+//
+//    public void setSurname(String surname) {
+//        this.surname = surname;
+//    }
+//
+//    public void setPeselNumber(String peselNumber) {
+//        this.peselNumber = peselNumber;
+//    }
+//
+//    public void setTelephoneNumber(String telephoneNumber) {
+//        this.telephoneNumber = telephoneNumber;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public void setStreet(String street) {
+//        street = street;
+//    }
+//
+//    public void setPostcode(String postcode) {
+//        this.postcode = postcode;
+//    }
+//
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
+
+
 }
