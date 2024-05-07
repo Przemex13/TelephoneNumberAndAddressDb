715f083248f1f9dc900e1f9fdd6faa079cfe5968
Przemyslaw Poskrobko <przemyslaw.poskrobko@gmail.com>
2024-05-07 20:44:53 +0200

    readme.md added polish and english

--- src/main/java/com/example/TelephoneNumberAndAddressDB/personalDetails/PersonalDetailRepository.java
+++ src/main/java/com/example/TelephoneNumberAndAddressDB/personalDetails/PersonalDetailRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonalDetailRepository extends JpaRepository<PersonalDetail, Integer> {
+public interface PersonalDetailRepository extends CrudRepository<PersonalDetail, Integer> {
 }
